88c5a566c7a7d6db97bf29e64507444bba96a2e4
Рафаэль Хусаинов <lolo123105@gmail.com>
2023-09-26 10:55:59 +0300

    First view made

--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.core import serializers
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Robot
+
+
+@csrf_exempt
+def ModelView(request):
+    """API-view for requests."""
+
+    if (request.method == 'GET'):
+        data = serializers.serialize('json', Robot.objects.all())
+        return JsonResponse(json.loads(data))
+
+    if (request.method == 'POST'):
+        body = json.loads(request.body.decode('utf-8'))
+        new_item = Robot.objects.create(serial=body['serial'],
+                                        model=body['model'],
+                                        version=body['version'],
+                                        created=body['created']
+                                        )
+        data = json.loads(serializers.serialize('json', [new_item]))
+        return JsonResponse(data)
